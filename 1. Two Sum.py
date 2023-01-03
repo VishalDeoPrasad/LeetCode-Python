acee8948d944d2fd3ce43eadb25940351f84cffd
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-03 22:39:48 +0530

    Solution 2

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,9 +1,18 @@
 class Solution:
-    #method 2
-    def twoSum(self, nums, target):
+    #Solution 1
+    def twoSum1(self, nums, target):
         pair = []
         [pair.append([i,j]) for i in range(len(nums)-1) for j in range(i+1, len(nums)) if nums[i]+nums[j] == target]
         return pair[0]
+    
+    #Solution 2
+    def twoSum(self, nums, target):
+        #solution 2
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return i,j
 
 s = Solution()
 print(s.twoSum([2,3,5,7], 10))
