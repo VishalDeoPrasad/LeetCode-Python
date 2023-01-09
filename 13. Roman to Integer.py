d12bbafea4896d362b67578d385a3cbab89cc8f1
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-10 01:43:43 +0530

    roman to integer

--- 13. Roman to Integer.py
+++ 13. Roman to Integer.py
@@ -3,7 +3,16 @@ class Solution:
         symbol_dict = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         return symbol_dict[symbol]
     
-    def romanToInt(self, s):  # "LVIII"
+    def romanToInt1(self, s):  # "LVIII"
+        result = self.valueof(s[-1])
+        n = len(s)
+        [result := result+self.valueof(s[i]) for i in range(-2, -n-1, -1) if self.valueof(s[i]) >= self.valueof(s[i+1]) else result := result-self.valueof(s[i])]
+            
+                
+            
+        return result
+    
+    def romanToInt1(self, s):  # "LVIII"
         result = self.valueof(s[-1])
         n = len(s)
         for i in range(-2, -n-1, -1):
