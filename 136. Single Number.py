25238d55826615fb26ba66d7182d9c5466fa1bdd
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-02-07 21:22:34 +0530

    singel number order of n

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,8 +1,13 @@
 class Solution:
-    def singleNumber(self, nums):
+    def singleNumber1(self, nums):
         for i in range(len(nums)):
             if nums.count(nums[i]) == 1:
                 return nums[i]
+    def singleNumber(self, nums):
+        unique = 0
+        for num in nums:
+            unique = unique ^ num
+        return unique
 
 nums = [4,1,2,1,2]
 print(Solution().singleNumber(nums))
