72d8bf7fa3a45d84c019b995f4f6d71295ba15bb
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-07 14:27:47 +0530

    power of four

---
+++ 342. Power of Four.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        while n>=4:
+            if n%4 != 0:
+                return False
+            n = n/4
+        return n==1
+    
+print(Solution().isPowerOfFour(16))
