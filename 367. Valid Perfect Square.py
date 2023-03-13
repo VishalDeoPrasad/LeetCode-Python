12fec59fd5e6e63df184266214ab716e832d69ce
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-13 13:38:48 +0530

    Valid Perfect Square

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -1,35 +1,37 @@
 class Solution:
     #idea 1 - Brute-force
     #TC - O(N), SC - O(1)
-    def solve1(self, A):
+    def solve(self, A):
+        if A == 1: return True
         for i in range(1, A):
             if i == A/i:
-                return A/i, True
-        return 0, False
+                return True
+        return False
     
     #idea 2 - Brute Force slightly better then previous
     #TC - O(N/2), SC - O(1)
     def solve1(self, A):
+        if A == 1: return True
         for i in range(1, A//2):
             if i == A/i:
                 return i, True
-        return 0, False
+        return False
     
     #idea 3 - Binary Search
     #TC - O(log N), SC - O(1)
-    def solve(self, num):
-        low = 0
-        high = num//2
-        while low <= high:
-            mid = (low+(high-low))//2
-            if mid == num//mid and num % mid == 0:
+    def isPerfectSquare(self, num):
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            #if mid * mid == num:
+            if mid == num/mid:
                 return True
-            
-            if mid < (num//mid):
-                low = mid + 1
+            elif mid * mid < num:
+                left = mid + 1
             else:
-                high = mid - 1
+                right = mid - 1
         return False
     
 print(Solution().solve1(1))
+print(Solution().isPerfectSquare(1))
         
