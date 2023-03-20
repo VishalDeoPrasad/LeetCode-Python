29adfca89e4575627ee7349bd1bb3809bf8abb08
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-20 15:57:51 +0530

    fizz buzz

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        game = []
+        for i in range(1, n+1):
+            if i%3 == 0 and i%5 == 0:
+                game.append("FizzBuzz")
+            elif i%3 == 0:
+                game.append("Fizz")
+            elif i%5 == 0:
+                game.append('Buzz')
+            else:
+                game.append(str(i))
+        return game
+
+print(Solution().fizzBuzz(5))
